d5cbfceb2eca7243b84eaf8ecd72b4fcd0947a6b
ayshajamjam <aj2604@columbia.edu>
2023-05-03 10:22:03 -0400

    Summary printed out only when last character sent/acked

--- node_new.py
+++ node_new.py
@@ -37,6 +37,8 @@ class Node:
         self.window_start = 0
         self.next_option = 0
 
+        self.message_length = 0
+
     def nodeListen(self, cond):
 
 
@@ -59,18 +61,19 @@ class Node:
             if(header == 'data'):
                 seqNum = int(lines[1])
                 data = lines[2]
+                endFlag = int(lines[3])
                 # Deterministic packet dropping
                 if(self.drop_method == '-d' and self.drop_value > 0 and (seqNum + 1) % self.drop_value == 0):
                 # if(self.packets_received < 3 and seqNum == 1):
                     print(">>> Dropping packet: ", seqNum)
                     self.test[(self.last_acked_packet + 1) % buffer_size] = 'X'
                     self.dropped_count += 1
-                    print(("[Summary] {}/{} packets discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
+                    # print(("[Summary] {}/{} packets discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
                 elif(self.drop_method == '-p' and random_num <= self.drop_value): # probabilistic
                     print("***Dropping packet: ", seqNum)
                     self.test[(self.last_acked_packet + 1) % buffer_size] = 'X'
                     self.dropped_count += 1
-                    print(("[Summary] {}/{} packets discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
+                    # print(("[Summary] {}/{} packets discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
                 elif(self.last_acked_packet != seqNum - 1):
                     print(("[{}] packet: {} content: {} discarded").format(time.time(), seqNum, data))
                     ack = 'ack\t' + str(self.last_acked_packet)
@@ -84,6 +87,9 @@ class Node:
                     self.last_acked_packet += 1
                     node_listen_socket.sendto(ack.encode(), (IP, self.peer_port))
                     print(("[{}] ACK: {} sent, expecting {}").format(time.time(), str(seqNum), str(seqNum + 1)))
+                    if(endFlag):
+                        print("LAST ACK SENT")
+                        print(("[Summary] {}/{} packets discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
                 print(">>> Testing Buffer: ", self.test)
             elif(header == 'ack'):
                 seqNum = int(lines[1])
@@ -93,7 +99,7 @@ class Node:
                     print("***Dropping an ack for packet: ", seqNum, "***")
                     self.test[(self.last_acked_packet + 1) % buffer_size] = 'X'
                     self.dropped_count += 1
-                    print(("[Summary] {}/{} acks discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
+                    # print(("[Summary] {}/{} acks discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
                     print(">>> Testing Buffer: ", self.test)
                     lock.release()
                 elif(self.drop_method == '-p' and random_num <= self.drop_value): # probabilistic
@@ -101,7 +107,7 @@ class Node:
                     print("***Dropping an ack for packet: ", seqNum, "***")
                     self.test[(self.last_acked_packet + 1 )% buffer_size] = 'X'
                     self.dropped_count += 1
-                    print(("[Summary] {}/{} acks discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
+                    # print(("[Summary] {}/{} acks discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
                     print(">>> Testing Buffer: ", self.test)
                     lock.release()
                 elif(self.last_acked_packet == seqNum):
@@ -124,10 +130,14 @@ class Node:
                     print(self.window_start, ' ', self.next_option)
                     print(">>> Testing Buffer: ", self.test)
                     lock.release()
+                    if(seqNum == self.message_length - 1):
+                        print("LAST CHAR RECEIVED")
+                        print(("[Summary] {}/{} acks discarded, loss rate = {}%").format(self.dropped_count, self.packets_received, float(self.dropped_count/self.packets_received)))
+
                 # print(">>> Testing Buffer: ", self.test)
 
     def timer(self, cond, node_send_socket):
-        global round2
+        # global round2
 
         cond.acquire()
         while True:
@@ -139,16 +149,21 @@ class Node:
             else:
                 print("Timeout- resend all packets in window: ", (self.window_start, self.next_option))
                 print("Last acked packet: ", self.last_acked_packet)
-                round2 = True
+                # round2 = True
                 if(self.window_start < self.next_option):
                     for i in range(self.window_start, self.next_option):
-                        packet = 'data\t' + str(self.sending_buffer[i][0]) + '\t' + self.sending_buffer[i][1]
+                        endFlag = 0
+                        if(i == (self.message_length - 1) % buffer_size):
+                            endFlag = 1
+                        packet = 'data\t' + str(self.sending_buffer[i][0]) + '\t' + self.sending_buffer[i][1] + '\t' + str(endFlag)
                         node_send_socket.sendto(packet.encode(), (IP, self.peer_port))
                         print(("[{}] packet: {} content: {} REsent").format(time.time(), self.sending_buffer[i][0], self.sending_buffer[i][1]))
                 elif(self.window_start > self.next_option):   # deals with wrap around case where window_start is at end of buffer and next_option is at the start
                     for i in range(self.window_start, self.next_option + buffer_size):
-                        print("Pay attention: ", i)
-                        packet = 'data\t' + str(self.sending_buffer[i % buffer_size][0]) + '\t' + self.sending_buffer[i % buffer_size][1]
+                        endFlag = 0
+                        if(i == (self.message_length - 1) % buffer_size):
+                            endFlag = 1
+                        packet = 'data\t' + str(self.sending_buffer[i % buffer_size][0]) + '\t' + self.sending_buffer[i % buffer_size][1] + '\t' + str(endFlag)
                         node_send_socket.sendto(packet.encode(), (IP, self.peer_port))
                         print(("[{}] packet: {} content: {} REsent").format(time.time(), self.sending_buffer[i % buffer_size][0], self.sending_buffer[i % buffer_size][1]))
 
@@ -187,6 +202,7 @@ class Node:
 
             # Retrieve message from input
             message = getMessage(input_list)
+            self.message_length = len(message)
 
             # Break message into single character packets
             i = 0
@@ -196,7 +212,11 @@ class Node:
                 if((self.next_option >= self.window_start and self.next_option - self.window_start < self.window_size)
                 or (self.next_option < self.window_start and (self.next_option + buffer_size) - self.window_start < self.window_size)):
 
-                    packet = 'data\t' + str(i) + '\t' + message[i]
+                    if (i == len(message) - 1):
+                        print("MAKING PACKET FOR LAST SEND")
+                        packet = 'data\t' + str(i) + '\t' + message[i] + '\t' + str(1)
+                    else:
+                        packet = 'data\t' + str(i) + '\t' + message[i] + '\t' + str(0)
                     
                     # Add packet to sending buffer
                     lock.acquire()
